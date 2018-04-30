cb574d410962f135c0d7d2bfb535c62ee17e668c
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-04-30 14:40:01 +0100

    Extract some settings to env.

--- flashmobber/settings.py
+++ flashmobber/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w8=$hn+)&b!k%@+uw0wq1masb3vjs-z81bc^b5zu2=z12!fa!e'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'false').lower() in {'on', 'true', 'yes', '1'}
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',') if 'ALLOWED_HOSTS' in os.environ else []
 
 
 # Application definition
